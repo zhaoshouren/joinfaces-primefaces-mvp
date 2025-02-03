6dcee13adba529d052a4310dd410d351a560f8af
Warren Chu <warren.chu@irs.gov>
2025-02-03 10:17:58 -0600

    initial impl

---
+++ src/main/java/org/zs/joinfaces_primefaces_mvp/JoinfacesPrimefacesMvpApplication.java
@@ -0,0 +1,13 @@
+package org.zs.joinfaces_primefaces_mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinfacesPrimefacesMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinfacesPrimefacesMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zs/joinfaces_primefaces_mvp/JoinfacesPrimefacesMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zs.joinfaces_primefaces_mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinfacesPrimefacesMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
